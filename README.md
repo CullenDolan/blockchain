a1d96522600ecffb00dbc297e0fa1ee7b493f276
cullen <cullen.dolan@gmail.com>
2021-04-20 09:37:39 -0500

    added the hash, last_block, and new_transaction functions. As well as updated readme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Trying to build a full app to deploy on AWS with the flask framework. Using [thi
 * **Hash** - A function that takes an input value and creates an output value in a deterministic way. The simplest example is *f(x) = y*. And y cannot be reerse engineered to get x.
 * **Block** - An object that typically includes an index, timestamp, list of transactions, a proof, and hash of previous block.
 * **Transactions** - Part of the block that include the sender, recipient, and amount exchanged.
+* **Genesis Block** - Very first blok in the chain that needs to be created manually.
+* **SHA-256** - Secure Hash Algorithm, computed with 8 32-bit words. THe current standard for encryption. This is important because most computers have a 32-bit or 64-bit architecture, and that is the amount of information a CPU can process each time it performs an operation
--- blockchain.py
+++ blockchain.py
@@ -1,32 +1,88 @@
-"""
-Primary file for creating the application.
+"""Primary file for creating the application."""
 
-"""
+import hashlib
+import json
+from time import time
+
 
 class Blockchain(object):
     def __init__(self):
-    '''create an empty list to store the blockchain'''
+        '''create an empty list to store the blockchain'''
         self.chain = []
         self.current_transactions = []
 
-
-    def new_block)(self):
-    '''create a new block and add it to a chain'''
-        pass
+        # create a genesis block 
+        self.new_block(previous_hash=1, proof=100)
 
 
-    def new_transaction(self):
-    '''store transactions on the block'''
-        pass
+    def new_block(self, proof, previous_hash):
+        '''
+        create a new block and add it to a chain
+        
+        Parameters:
+        -----------
+        proof: <int> The proof given by proof of work algorithm
+        previous_hash: <str> optional hash of previous block
+
+        Return:
+        -------
+        <dict> New Block  
+        '''
+        block = {
+            'index':len(self.chain) + 1,
+            'timestamp': time(),
+            'transaction': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+
+    def new_transaction(self, sender, recipient, amount):
+        '''
+        Store transactions on the block
+        
+        Parameters:
+        -----------
+        sender: <str> Address of the sender
+        recipient:  <str> address of person receiving the amount
+        amount: <int> value of the transaction 
+        
+        Return:
+        -------
+        <int> the index of the block that holds the current transaction
+        '''
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
 
     
     @staticmethod
     def hash(block):
-    '''hashes the block'''
-        pass
+        '''
+        Hashes the block with SHA-256
+        
+        Parameters:
+        -----------
+        block: <dict> dictionary of the full block  
+        
+        Returns:
+        --------
+        <str> Output of the hash function
+        '''
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
     
-    
+
     @property
     def last_block(self):
-    '''returns the last block in the chain'''
-        pass
+        '''returns the last block in the chain'''
+        return self.chain[-1]
